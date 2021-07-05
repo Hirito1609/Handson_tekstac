bdc64fb43b31f4f332fc1ed7292038fed3b846a3
Hariharan V <hariharanvenkey@gmail.com>
2021-07-05 13:17:35 +0530

    logging

---
+++ ASP.NetCore-Handson 3/Logging/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Logging.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILog log = LogManager.GetLogger("mylog");
+        public ActionResult Index()
+        {
+            
+            log.Info("This is an information message");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            log.Debug("This is a debug message");
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            log.Warn("This is a warning message");
+            return View();
+        }
+    }
+}
