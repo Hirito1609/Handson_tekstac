e7c37d43c9db71393173da6875581862675dd0b5
Hariharan V <hariharanvenkey@gmail.com>
2021-07-05 09:59:08 +0530

    codeanalysis-Handson

---
+++ CodeAnalysis-Handson 1/CodeAnalysis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = { "John", "Boby", "Chraig", "Denis", "Alfee", "Bond" };
+
+            //var namesOfB = names.Where(n => n.StartsWith("B"));
+
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = a / b;
+            }
+            catch(Exception ex)
+            { 
+            
+            }
+        }
+    }
+}
